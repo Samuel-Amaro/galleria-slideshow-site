f589b4cc4b26c762388802836aa0259b59290498
Samuel Amaro <samuelamaro96746313@gmail.com>
2024-01-04 21:53:09 -0300

    feat: add types api

---
+++ src/types/api.ts
@@ -0,0 +1,33 @@
+export interface API {
+	pagination: Pagination;
+	data: Data[];
+	config: {
+		iiif_url: string;
+	};
+}
+
+export type Pagination = {
+	total: number;
+	limit: number;
+	offset: number;
+	total_pages: number;
+	current_page: number;
+	next_url: string;
+};
+
+export type Data = {
+	id: number;
+	title: string;
+	thumbnail: {
+		lqip: string;
+		width: number;
+		height: number;
+		alt_text: string;
+	};
+	date_start: number;
+	date_end: number;
+	description: string;
+	artist_id: number;
+	artist_title: string;
+	image_id: string;
+};
